e9959d359b91defee88aa19fd8b43881219b59fb
Sid Chaudhary <sidchaudhary@users.noreply.github.com>
2020-05-09 08:32:27 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 #git push -f git@github.com:intempt/intempt.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:intempt/help-docs.git master:gh-pages
+git push -f git@github.com:intempt/help.git master:gh-pages
 
 cd -
